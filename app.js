77f341836f5f299e63704878be47593339fdb545
root <root@localhost.localdomain>
2018-06-11 13:15:50 +0800

    fix middleware

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const api = require('./routes/api.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/AngularStarter0')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/AngularStarter0/index.html'));
+});
+
+module.exports = app;
